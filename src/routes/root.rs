e0e28e604ddeb8b77b06b49dc0aa78ddb5912e25
Austin Rooks <Austionian@users.noreply.github.com>
2024-03-26 22:06:01 -0500

    break out latest

--- src/routes/root.rs
+++ src/routes/root.rs
@@ -7,27 +7,66 @@ use axum::{
 use chrono::{DateTime, Local, TimeZone};
 use std::sync::Arc;
 
-#[derive(serde::Serialize, serde::Deserialize, Debug)]
-struct Forecast {
-    properties: Properties,
+struct Latest {
+    as_of: String,
+    wind_direction: String,
+    wind_speed: String,
+    gusts: String,
 }
 
-#[derive(serde::Serialize, serde::Deserialize, Debug)]
-#[serde(rename_all = "kebab-case")]
-struct Properties {
-    wave_height: String,
+impl Latest {
+    async fn get() -> anyhow::Result<Self> {
+        let data = reqwest::get("https://www.ndbc.noaa.gov/data/realtime2/MLWW3.txt")
+            .await?
+            .text()
+            .await?;
+
+        let latest = data.lines().collect::<Vec<_>>();
+        let latest = latest.get(2).unwrap();
+
+        let (as_of, measurements) = latest.split_at(16);
+
+        let as_of = Self::parse_as_of(as_of)?;
+        let mut measurements = measurements.trim().split_whitespace();
+        let wind_direction = measurements.next().unwrap().to_string();
+
+        let wind_speed = measurements.next().unwrap().to_string();
+        let gusts = measurements.next().unwrap().to_string();
+
+        Ok(Self {
+            as_of,
+            wind_direction,
+            wind_speed,
+            gusts,
+        })
+    }
+
+    fn parse_as_of(as_of: &str) -> anyhow::Result<String> {
+        let as_of = as_of.trim().split(" ").collect::<Vec<_>>();
+        let as_of: DateTime<Local> = chrono::Utc
+            .with_ymd_and_hms(
+                as_of.get(0).unwrap().parse::<i32>().unwrap(),
+                as_of.get(1).unwrap().parse::<u32>().unwrap(),
+                as_of.get(2).unwrap().parse::<u32>().unwrap(),
+                as_of.get(3).unwrap().parse::<u32>().unwrap(),
+                as_of.get(4).unwrap().parse::<u32>().unwrap(),
+                00,
+            )
+            .unwrap()
+            .try_into()
+            .unwrap();
+
+        let as_of = as_of.to_rfc2822();
+
+        Ok(as_of.split(" -").next().unwrap().to_string())
+    }
 }
 
 /// Handler to return the website's index
 pub async fn root(State(state): State<Arc<AppState>>) -> Result<Html<String>, AppError> {
     let mut context = tera::Context::new();
 
-    context.insert("title", &state.title);
-
-    let data = reqwest::get("https://www.ndbc.noaa.gov/data/realtime2/MLWW3.txt")
-        .await?
-        .text()
-        .await?;
+    let latest = Latest::get().await?;
 
     let client = reqwest::Client::builder()
         .user_agent("GatheringSurf/0.1 (+https://gathering.surf)")
@@ -60,38 +99,11 @@ pub async fn root(State(state): State<Arc<AppState>>) -> Result<Html<String>, Ap
             .unwrap()
     );
 
-    let latest = data.lines().collect::<Vec<_>>();
-    let latest = latest.get(2).unwrap();
-
-    let (time, measurements) = latest.split_at(16);
-
-    let time = time.trim().split(" ").collect::<Vec<_>>();
-    let time: DateTime<Local> = chrono::Utc
-        .with_ymd_and_hms(
-            time.get(0).unwrap().parse::<i32>().unwrap(),
-            time.get(1).unwrap().parse::<u32>().unwrap(),
-            time.get(2).unwrap().parse::<u32>().unwrap(),
-            time.get(3).unwrap().parse::<u32>().unwrap(),
-            time.get(4).unwrap().parse::<u32>().unwrap(),
-            00,
-        )
-        .unwrap()
-        .try_into()
-        .unwrap();
-
-    let mut measurements = measurements.trim().split_whitespace();
-    let wind_direction = measurements.next().unwrap();
-
-    let wind_speed = measurements.next().unwrap();
-    let gusts = measurements.next().unwrap();
-
-    let mut time = time.to_rfc2822();
-    time = time.split(" -").next().unwrap().to_string();
-
-    context.insert("time", &time);
-    context.insert("wind_direction", wind_direction);
-    context.insert("wind_speed", wind_speed);
-    context.insert("gusts", gusts);
+    context.insert("title", &state.title);
+    context.insert("as_of", &latest.as_of);
+    context.insert("wind_direction", &latest.wind_direction);
+    context.insert("wind_speed", &latest.wind_speed);
+    context.insert("gusts", &latest.gusts);
 
     match TEMPLATES.render("index.html", &context) {
         Ok(s) => Ok(Html(s)),
