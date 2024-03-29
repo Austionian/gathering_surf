b09784cd043c67e34bc40fb8e5b9975fbeae2c37
Austin Rooks <Austionian@users.noreply.github.com>
2024-03-29 15:01:38 -0500

    add wave height chart to root

--- src/routes/root.rs
+++ src/routes/root.rs
@@ -6,8 +6,7 @@ use axum::{
     response::{Html, IntoResponse, Response},
 };
 use chrono::{DateTime, Local, TimeZone};
-use std::{str::FromStr, sync::Arc};
-use tracing::Value;
+use std::sync::Arc;
 
 struct Latest {
     as_of: String,
@@ -65,9 +64,72 @@ impl Latest {
 }
 
 #[derive(Debug)]
+struct WaveHeightData(Vec<WaveHeightForecast>);
+
+impl WaveHeightData {
+    fn get_data(&self) -> String {
+        self.0
+            .iter()
+            .map(|data| format!("{:.3},", data.value * 3.281))
+            .collect()
+    }
+
+    fn get_labels(&self) -> String {
+        self.0
+            .iter()
+            .map(|data| format!("'{}',", data.valid_time))
+            .collect()
+    }
+}
+
+#[derive(Debug)]
 struct WaveHeightForecast {
     value: f64,
     valid_time: String,
+}
+
+fn parse_hour(s: &str) -> anyhow::Result<usize> {
+    if let Some((_, hour)) = s.split_once("T") {
+        let hour = hour.strip_suffix("H\"").ok_or(anyhow!("no hour found!"))?;
+        return Ok(hour.parse()?);
+    };
+
+    Err(anyhow!("no T found!"))
+}
+
+fn increment_time(t: &str, amount: usize) -> anyhow::Result<String> {
+    let time = t.strip_prefix("\"").unwrap();
+    let time: DateTime<Local> = chrono::DateTime::parse_from_str(time, "%+").unwrap().into();
+
+    Ok((time + std::time::Duration::from_secs(amount as u64 * 3_600)).to_string())
+}
+
+impl WaveHeightForecast {
+    fn expand_time_ranges(&self) -> anyhow::Result<Vec<Self>> {
+        let (time, period) = self
+            .valid_time
+            .split_once("/P")
+            .ok_or(anyhow!("Unknown period found!"))?;
+
+        let mut total = 0;
+        if let Some((day, hour)) = period.split_once("D") {
+            total += day.parse::<usize>().unwrap() * 24;
+            total += parse_hour(hour)?;
+        } else {
+            total += parse_hour(period)?;
+        };
+
+        let mut out = Vec::with_capacity(total);
+
+        for i in 0..total {
+            out.push(Self {
+                value: self.value.clone(),
+                valid_time: increment_time(time, i)?,
+            })
+        }
+
+        Ok(out)
+    }
 }
 
 impl From<serde_json::Value> for WaveHeightForecast {
@@ -90,34 +152,39 @@ pub async fn root(State(state): State<Arc<AppState>>) -> Result<Html<String>, Ap
         .build()
         .unwrap();
 
+    // milwauke = "https://api.weather.gov/gridpoints/MKX/91,67"
     let forecast = client
-        .get("https://api.weather.gov/gridpoints/MKX/91,67")
+        .get("https://api.weather.gov/gridpoints/MKX/90,67")
         .send()
         .await?
         .json::<serde_json::Value>()
         .await?;
 
-    let wave_heights = forecast
-        .get("properties")
-        .ok_or(anyhow!("no properties found!"))?
-        .get("waveHeight")
-        .ok_or(anyhow!("no wave heights found!"))?
-        .get("values")
-        .ok_or(anyhow!("no values found!"))?
-        .as_array()
-        .ok_or(anyhow!("array not found!"))?
-        .clone()
-        .into_iter()
-        .map(WaveHeightForecast::from)
-        .collect::<Vec<_>>();
-
-    println!("{:?}", wave_heights);
+    let wave_heights = WaveHeightData(
+        forecast
+            .get("properties")
+            .ok_or(anyhow!("no properties found!"))?
+            .get("waveHeight")
+            .ok_or(anyhow!("no wave heights found!"))?
+            .get("values")
+            .ok_or(anyhow!("no values found!"))?
+            .as_array()
+            .ok_or(anyhow!("array not found!"))?
+            .clone()
+            .into_iter()
+            .map(WaveHeightForecast::from)
+            .map(|w| WaveHeightForecast::expand_time_ranges(&w).unwrap())
+            .flatten()
+            .collect::<Vec<_>>(),
+    );
 
     context.insert("title", &state.title);
     context.insert("as_of", &latest.as_of);
     context.insert("wind_direction", &latest.wind_direction);
     context.insert("wind_speed", &latest.wind_speed);
     context.insert("gusts", &latest.gusts);
+    context.insert("wave_height_data", &wave_heights.get_data());
+    context.insert("wave_height_labels", &wave_heights.get_labels());
 
     match TEMPLATES.render("index.html", &context) {
         Ok(s) => Ok(Html(s)),
