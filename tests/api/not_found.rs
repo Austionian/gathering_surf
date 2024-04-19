c9f361f062184d7adfbaf927b1bf0d6ec6944fbd
Austin Rooks <Austionian@users.noreply.github.com>
2024-04-19 11:31:57 -0500

    add test for 404

---
+++ tests/api/not_found.rs
@@ -0,0 +1,19 @@
+use crate::helpers::start_test_app;
+
+#[tokio::test]
+async fn an_invalid_url_returns_the_404_not_found() {
+    let addr = start_test_app()
+        .await
+        .expect("Unable to start test server.");
+
+    let response = reqwest::get(format!("http://{}/four-oh-four-please", &addr))
+        .await
+        .unwrap();
+
+    assert_eq!(response.status().as_u16(), 404);
+    assert!(response
+        .text()
+        .await
+        .unwrap()
+        .contains("Nothing to see here"));
+}
