6018bf98f34cf1c18b359df27f4a71bc752ba17b
Austin Rooks <Austionian@users.noreply.github.com>
2024-04-22 22:44:37 -0500

    latest test

---
+++ tests/api/latest.rs
@@ -0,0 +1,22 @@
+use crate::helpers::start_test_app;
+
+#[tokio::test]
+async fn it_returns_the_latest_data_as_json() {
+    let addr = start_test_app()
+        .await
+        .expect("Unable to start test server.");
+
+    let response = reqwest::get(format!("http://{}/api/latest", &addr))
+        .await
+        .unwrap();
+
+    assert_eq!(response.status().as_u16(), 200);
+
+    let data = response.text().await.unwrap();
+
+    assert!(data.contains("as_of"));
+    assert!(data.contains("wind_direction"));
+    assert!(data.contains("air_temp"));
+    assert!(data.contains("quality_color"));
+    assert!(data.contains("quality_text"));
+}
--- tests/api/main.rs
+++ tests/api/main.rs
@@ -1,4 +1,5 @@
 mod health_check;
 mod helpers;
+mod latest;
 mod not_found;
 mod root;
