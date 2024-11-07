32f217ed99c082975e3ab218eaf0fdd716a9a7cf
Austin Rooks <austin@r00ks.io>
2024-11-06 21:56:38 -0600

    increase cache length

--- src/utils/redis_utils.rs
+++ src/utils/redis_utils.rs
@@ -7,13 +7,13 @@ pub async fn get(key: &str, pool: &Pool<RedisConnectionManager>) -> Option<Strin
     pool.get().await.ok()?.get(key).await.ok()
 }
 
-/// Sets the given k,v pair for 100 seconds in the Redis cache.
+/// Sets the given k,v pair for 5 minutes in the Redis cache.
 pub async fn set(
     key: &str,
     value: &str,
     pool: &Pool<RedisConnectionManager>,
 ) -> anyhow::Result<()> {
-    let opts = SetOptions::default().with_expiration(SetExpiry::EX(100));
+    let opts = SetOptions::default().with_expiration(SetExpiry::EX(300));
     pool.get()
         .await?
         .set_options::<&str, &str, ()>(key, value, opts)
