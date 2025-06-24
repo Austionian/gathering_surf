5f07916b0a0cbb8596ed9087b84dd0e33b5b16bc
Austin Rooks <austin@r00ks.io>
2025-06-23 20:41:00 -0500

    update deps

--- src/routes/watch.rs
+++ src/routes/watch.rs
@@ -7,6 +7,7 @@ use std::sync::Arc;
 
 pub async fn watch(ws: WebSocketUpgrade, State(state): State<Arc<AppState>>) -> impl IntoResponse {
     let mut rx = state.event_stream.clone().subscribe();
+
     ws.on_upgrade(move |mut socket| async move {
         tracing::info!("new connection, watching");
         if rx.recv().await.is_ok() {
